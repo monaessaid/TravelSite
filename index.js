b29ea716535738d088b949c87fc3f71cfe433f92
Mona <2829922e@student.gla.ac.uk>
2024-08-04 16:37:11 -0700

    arrays

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let africaList = []
+let asiaList = []
+let carribeanList = []
+let centralAmericaList = []
+let northAmericaList = []
+let southAmericaList = []
+let europeList = []
+let oceaniaList = []
