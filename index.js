56b7329534e0c0ea098281284655f0ba5bc94750
Mona <2829922e@student.gla.ac.uk>
2024-08-04 19:30:11 -0700

    Africa

--- index.js
+++ index.js
@@ -1,5 +1,76 @@
 
-let africaList = []
+let antarcticaList = ["Antarctica"]
+
+let africaList = [
+"Algeria" ,
+"Angola" ,
+"Benin" ,
+"Botswana" ,
+"British Indian Ocean Territory" ,
+"Burkina Faso" ,
+"Burundi" ,
+"Cabo Verde" ,
+"Cameroon" ,
+"Central African Republic" ,
+"Chad" ,
+"Comoros" ,
+"Congo" ,
+"Côte d’Ivoire" ,
+"Democratic Republic of the Congo" ,
+"Djibouti" ,
+"Egypt" ,
+"Equatorial Guinea" ,
+"Eritrea" ,
+"Eswatini" ,
+"Ethiopia" ,
+"French Southern Territories" ,
+"Gabon" ,
+"Gambia" ,
+"Ghana" ,
+"Guinea" ,
+"Guinea-Bissau" ,
+"Kenya" ,
+"Lesotho" ,
+"Liberia" ,
+"Libya" ,
+"Madagascar" ,
+"Malawi" ,
+"Mali" ,
+"Mauritania" ,
+"Mauritius" ,
+"Mayotte" ,
+"Morocco" ,
+"Mozambique" ,
+"Namibia" ,
+"Niger" ,
+"Nigeria" ,
+"Réunion" ,
+"Rwanda" ,
+"Saint Helena" ,
+"Sao Tome and Principe" ,
+"Senegal" ,
+"Seychelles" ,
+"Sierra Leone" ,
+"Somalia" ,
+"South Africa" ,
+"South Sudan" ,
+"Sudan" ,
+"Togo" ,
+"Tunisia" ,
+"Uganda" ,
+"United Republic of Tanzania" ,
+"Western Sahara" ,
+"Zambia" ,
+"Zimbabwe"
+]
+
+
+
+
+
+
+
+
 let asiaList = []
 let carribeanList = []
 let centralAmericaList = []
